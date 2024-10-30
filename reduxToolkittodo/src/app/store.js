003470e9f3289aeeb2783474310c9e7fd0d88d07
Abdullah Ansari <143590602+ab6955@users.noreply.github.com>
2024-10-30 07:12:34 +0000

    Redux Start

---
+++ reduxToolkittodo/src/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import todoSlice from "../features/todo/todoSlice";
+import todoReducer from '../features/todo/todoSlice';
+
+export const store = configureStore({
+    reducer:todoReducer
+});
