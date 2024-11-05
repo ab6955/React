bc6670065902bed4d211dc015d25c6514a42ed30
Abdullah Ansari <143590602+ab6955@users.noreply.github.com>
2024-11-05 12:31:56 +0000

    configure redux toolkit in big projects

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
 
 function App() {
-  // console.log(process.env.REACT_APP_APPWRITE_URL)
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>A blog App with Appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>TODO:{/* <outlet/> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
