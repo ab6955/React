c2c84951b75d74e0a45bd3187de2b7a9a33aa536
Abdullah Ansari <143590602+ab6955@users.noreply.github.com>
2024-11-19 12:26:59 +0000

    adding form and slug values

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import Logoutbtn from "./Header/Logoutbtn";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, Logoutbtn };
+export { Header, Footer, Container, Logo, Logoutbtn, RTE };
